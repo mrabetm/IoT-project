55301eea4b489bc66f16e5ce1320be9284ae1f7a
Mortada M'Rabet <mortada.mrabet@hva.nl>
2022-02-18 10:49:19 +0100

    connect to mongo database is succesfull

--- dice_website_backend/src/main/java/com/iot_mrabetm/dice_website_backend/repository/RollRepository.java
+++ dice_website_backend/src/main/java/com/iot_mrabetm/dice_website_backend/repository/RollRepository.java
@@ -1,11 +1,12 @@
 package com.iot_mrabetm.dice_website_backend.repository;
 
 import com.iot_mrabetm.dice_website_backend.models.Roll;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface RollRepository extends CrudRepository<Roll, Integer> {
+public interface RollRepository extends MongoRepository<Roll, Integer> {
     List<Roll> findAll();
 
     Roll findById(int id);

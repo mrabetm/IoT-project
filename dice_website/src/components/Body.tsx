e707929417c1893079119c2d2264fc4a3978f13c
Mortada M'Rabet <mortada.mrabet@hva.nl>
2022-03-11 16:47:15 +0100

    now shows picture reminisent of what has been rolled

--- dice_website/src/components/Body.tsx
+++ dice_website/src/components/Body.tsx
@@ -2,6 +2,13 @@ import React, {Component, useState} from "react";
 import axios from "axios";
 import Api from "../Route";
 import {Button, Card} from "react-bootstrap";
+import img1 from "../styles/resources/dice1.gif";
+import img2 from "../styles/resources/dice2.png";
+import img3 from "../styles/resources/dice3.png";
+import img4 from "../styles/resources/dice4.png";
+import img5 from "../styles/resources/dice5.png";
+import img6 from "../styles/resources/dice6.gif";
+
 
 class Body extends Component<any, any>{
 
@@ -12,13 +19,7 @@ class Body extends Component<any, any>{
         this.state = {
             rolls: []
         }
-        console.log(this.state)
-
     }
-
-    // showPictureBasedOnScore(){
-    //     if (this.state.rolls )
-    // }
 
     componentDidMount() {
         this.getLatestRoll();
@@ -36,26 +37,38 @@ class Body extends Component<any, any>{
         })
     }
 
+    showPictureBasedOnScore(): string{
+        switch (this.state.rolls["score"]){
+            case 1:
+                return img1;
+            case 2:
+                return img2;
+
+            case 3:
+                return img3;
+
+            case 4:
+                return img4;
+
+            case 5:
+                return img5;
+            case 6:
+                return img6;
+        }
+        return "";
+    }
+
     render(){
+        console.log(this.showPictureBasedOnScore())
         return (
             <div id="container">
                 <div id="latestRollContainer">
-                {/*<ul>*/}
-                {/*    {this.state.rolls*/}
-                {/*            .map(roll =>*/}
-                {/*                <li key={roll['id']}>you have rolled {roll['score']}</li>*/}
-                {/*        )*/}
-                {/*    }*/}
-                {/*</ul>*/}
-
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180"/>
+                    <Card.Img id="card-img" variant="top" src={this.showPictureBasedOnScore()}/>
                     <Card.Body>
-                        <Card.Title>You rolled {}
-                        </Card.Title>
+                        <Card.Title>You rolled {this.state.rolls["score"]}</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            You rolled this number on {this.state.rolls["date"]}
                         </Card.Text>
                     </Card.Body>
                 </Card>
